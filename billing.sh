4977d0ce6399aedb911266a52fd65b63f774177b
subramanyam499 <subramanyam499@gmail.com>
2024-01-03 08:52:44 -0800

    test

--- billing.sh
+++ billing.sh
@@ -1,3 +1,4 @@
 echo 'this month electricity bill'
 echo 'the bill amount is 999'
 echo 'amount paid'
+echo 'no due'
--- tang.sh
+++ tang.sh
@@ -1,2 +1,3 @@
 echo 'the tango domain'
 echo 'the domain is not available'
+echo 'the domain is available'
